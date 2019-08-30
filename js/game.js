0986927aee10bcf99aeabe456cd1d22f4aabe64e
zharfan akbar <zharfan.akbar104@gmail.com>
2019-08-31 04:34:27 +0700

    a

---
+++ js/game.js
@@ -0,0 +1,24 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 480,
+    height: 640,
+    backgroundColor: "black",
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    scene: [
+        SceneMainMenu,
+        SceneMain,
+        SceneGameOver
+
+    ],
+    pixelArt: true,
+    roundPixels: true
+};
+var game = new Phaser.Game(config);
